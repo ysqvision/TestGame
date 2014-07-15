cb5a42b73d97864ae659ac7707daba9088f16e08
Jieming Mao <maojm@nat-oitwireless-inside-vapornet100-c-22356.princeton.edu>
2014-07-15 13:22:23 -0400

    Initial Commit

---
+++ TestGame/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  TestGame
+//
+//  Created by Jieming Mao on 14-7-15.
+//  Copyright Jieming Mao 2014年. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ TestGame/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestGame
+//
+//  Created by Jieming Mao on 14-7-15.
+//  Copyright Jieming Mao 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
